244ed3c300e3f6f2c3861a8ade7374429c7e59a5
rahul-aerospike <58450859+rahul-aerospike@users.noreply.github.com>
2021-04-22 09:38:56 -0700

    Update project to run with spark 3.0.0 and connector 3.0.1 (#4)
    * Update project to run with spark 3.0.0 and connector 3.0.1
    
    * Added warning.
    
    Co-authored-by: Joe M <jmartin@aerospike.com>

--- src/main/scala/com/aerospike/spark/aeroJoinExample.scala
+++ src/main/scala/com/aerospike/spark/aeroJoinExample.scala
@@ -1,57 +1,63 @@
 package com.aerospike.spark
 
-import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
-
+import scala.collection.mutable
 /**
- * This example will load some data into a "Business" namespace in an aerospike database.
+ * This example will load some data into specified namespace(by default test) in an specified aerospike database (by default running localhost:3000).
  * It will then use aeroJoin to take a sequence of ids and load the appropriate customer data, filter it, and print out the result.
  *
  * Prereqs: A working Aerospike Connect for Spark and an Aerospike server running on default port on localhost with at least 1 namespace named "Business"
  */
-object aeroJoinExample extends LazyLogging with Serializable {
-  val conf: SparkConf = new SparkConf()
-    .setAppName("AeroJoin")
-    .set("aerospike.seedhost", "localhost")
-    .set("aerospike.port", "3000")
-    .set("aerospike.namespace", "Business")
-
-  val session: SparkSession = SparkSession.builder()
-    .config(conf)
-    .master("local[*]")
-    .appName("Aerospike Example(II)")
-    .config("spark.ui.enabled", "false")
-    .getOrCreate()
+object aeroJoinExample {
 
   def main(args: Array[String]) {
+
+    val commandLineParams = mutable.Map[String, String]()
+    for (item <- args.grouped(2))
+      commandLineParams(item(0).trim()) = item(1).trim
+    val allParams=commandLineParams.toMap
+
+    val conf: SparkConf = new SparkConf()
+      .setAppName("AeroJoin")
+      .set("aerospike.seedhost", allParams.getOrElse("aerospike.seedhost","localhost:3000") )
+      .set("aerospike.namespace", allParams.getOrElse("aerospike.namespace","test"))
+
+    val session: SparkSession = SparkSession.builder()
+      .config(conf)
+      .master("local[*]")  //change it, if your spark cluster is not local.
+      .config("spark.ui.enabled", "false")
+      .getOrCreate()
+
+    session.sparkContext.setLogLevel("error")
     import session.implicits._
+    loadCustomerData(session)
 
-    loadCustomerData()
-
-    val ids = Seq("customer1", "customer2",
-      "customer3", "customer4", "IDontExist")
-
+    val ids = Seq("customer1", "customer2", "customer3", "customer4", "IDontExist")
+    //convert Dataframe to Dataset. This step is needed since aeroJoin is defined over Dataset.
     val customerIdsDF = ids.toDF("customer_id").as[CustomerID]
 
     val customerDS = customerIdsDF.aeroJoin[CustomerKV]("customer_id", "Customers")
     customerDS.foreach(b => println(b))
-
     val bestCustomers = customerDS.filter(customer => customer.stars > 4)
     bestCustomers.foreach(b => println(b))
-
-    bestCustomers.map(c => new Customer(c.key, c.customer_id, c.first, c.last, c.stars)).toDF("key", "customer_id", "last", "first", "stars").
-      write.mode(SaveMode.Overwrite).
-      format("com.aerospike.spark.sql").
+    bestCustomers.map(c => Customer(c.key, c.customer_id, c.first, c.last, c.stars)).toDF("key", "customer_id", "last", "first", "stars").
+      write.
+      mode(SaveMode.Overwrite).
+      format("aerospike").
       option("aerospike.updateByKey", "customer_id").
       option("aerospike.set", "BestCustomers").
       save()
     session.stop()
   }
 
-  def loadCustomerData(): Unit = {
+  /**
+   * Save some sample data in Customers set for experimentation.
+   */
+  def loadCustomerData(session: SparkSession): Unit = {
 
+    import session.implicits._
     val schema: StructType = new StructType(Array(
       StructField("key", StringType, nullable = true),
       StructField("customer_id", StringType, nullable = false),
@@ -73,9 +79,9 @@ object aeroJoinExample extends LazyLogging with Serializable {
 
     customerDF.write.
       mode(SaveMode.Overwrite).
-      format("com.aerospike.spark.sql").
+      format("aerospike").
       option("aerospike.updateByKey", "customer_id").
-      option("aerospike.set", "Customers").
+      option("aerospike.set", "Customers").//insert into this set
       save()
   }
 }
